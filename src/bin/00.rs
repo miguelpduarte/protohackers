fcfd130e0ba0550457df8f713dd0ea8bcaafa9c2
miguelpduarte <miguelpduarte98@gmail.com>
2024-05-27 14:46:56 +0200

    Add smoke test

---
+++ src/bin/00.rs
@@ -0,0 +1,38 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("localhost:8008").await?;
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+
+        println!("got connection from {}", addr);
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                println!("got {} bytes", buf.len());
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
+}
